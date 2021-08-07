2ad96405c659ea6093ee6415876f0ebca5bf64de
Aza Maulana Hadi <azamaulanaaa@gmail.com>
2021-08-07 08:54:50 +0700

    feat(tranports): add gRPC client library

--- transport/grpc.go
+++ transport/grpc.go
@@ -5,8 +5,11 @@ import (
 
 	"github.com/cerminan/torrent/endpoints"
 	"github.com/cerminan/torrent/pb"
+	"github.com/cerminan/torrent/service"
+	"github.com/go-kit/kit/endpoint"
 	"github.com/go-kit/kit/log"
 	gt "github.com/go-kit/kit/transport/grpc"
+	"google.golang.org/grpc"
 )
 
 type gRPCServer struct {
@@ -14,6 +17,33 @@ type gRPCServer struct {
   files gt.Handler
   readat gt.Handler
   pb.UnimplementedTorrentServer
+}
+
+func NewGRPCClient(conn *grpc.ClientConn) service.Service {
+  var files endpoint.Endpoint
+  files = gt.NewClient(
+    conn,
+    "Torrent",
+    "Files",
+    encodeFilesRequest,
+    decodeFilesResponse,
+    pb.FilesRes{},
+  ).Endpoint()
+
+  var readAt endpoint.Endpoint
+  readAt = gt.NewClient(
+    conn,
+    "Torrent",
+    "ReadAt",
+    encodeReadAtRequest,
+    decodeReadAtResponse,
+    pb.ReadAtRes{},
+  ).Endpoint()
+
+  return endpoints.Endpoints{
+    FilesEndpoint: files,
+    ReadAtEndpoint: readAt, 
+  }
 }
 
 func NewGRPCServer(endpoints endpoints.Endpoints, logger log.Logger) pb.TorrentServer {
@@ -42,6 +72,12 @@ func (s *gRPCServer) Files(ctx context.Context, req *pb.FilesReq) (*pb.FilesRes,
   return res.(*pb.FilesRes), nil
 }
 
+func encodeFilesRequest(_ context.Context, request interface{}) (interface{}, error){
+  var req endpoints.FilesReq
+  req = request.(endpoints.FilesReq)
+  return &pb.FilesReq{Magnet: req.Magnet}, nil
+}
+
 func decodeFilesRequest(_ context.Context, request interface{}) (interface{}, error) {
   var req *pb.FilesReq
   req = request.(*pb.FilesReq)
@@ -63,6 +99,22 @@ func encodeFilesRespones(_ context.Context, response interface{}) (interface{}, 
   return &pb.FilesRes{Files: files}, nil
 }
 
+func decodeFilesResponse(_ context.Context, response interface{}) (interface{}, error){
+  var res *pb.FilesRes
+  res = response.(*pb.FilesRes)
+
+  var files []endpoints.File
+  for _, file := range res.Files {
+    files = append(files, endpoints.File{
+      TorrentHash: file.TorrentHash,
+      Name: file.Name,
+      Length: file.Length,
+    })
+  }
+
+  return endpoints.FilesRes{Files: files}, nil
+}
+
 func (s *gRPCServer) ReadAt(ctx context.Context, req *pb.ReadAtReq) (*pb.ReadAtRes, error) {
   var err error
   var resp interface{}
@@ -72,6 +124,20 @@ func (s *gRPCServer) ReadAt(ctx context.Context, req *pb.ReadAtReq) (*pb.ReadAtR
   }
 
   return resp.(*pb.ReadAtRes), nil
+}
+
+func encodeReadAtRequest(_ context.Context, request interface{}) (interface{}, error) {
+  var req endpoints.ReadAtReq
+  req = request.(endpoints.ReadAtReq)
+  
+  var reqFile *pb.File
+  reqFile = &pb.File{
+    TorrentHash: req.File.TorrentHash,
+    Name: req.File.Name,
+    Length: req.File.Length,
+  }
+
+  return &pb.ReadAtReq{File: reqFile, Off: req.Off, Ln: req.Ln}, nil
 }
 
 func decodeReadAtRequest(_ context.Context, request interface{}) (interface{}, error) {
@@ -94,3 +160,8 @@ func endcodeReadAtResponse(_ context.Context, response interface{}) (interface{}
   return &pb.ReadAtRes{Buffer: res.Buffer}, nil
 }
 
+func decodeReadAtResponse(_ context.Context, response interface{}) (interface{}, error) {
+  var res *pb.ReadAtRes
+  res = response.(*pb.ReadAtRes)
+  return endpoints.ReadAtRes{Buffer: res.Buffer}, nil
+}
